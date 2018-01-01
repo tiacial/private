8e6ecd12758243e61d5d18c09fb49723bbb3d390
Zhang <tianxuz@amazon.com>
2018-01-01 12:33:14 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" floating temporary absolute clipped app>
+      <v-list>
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i">
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn v-for="(item, i) in items" :key="i">
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+            
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span class="text-xs-center">&copy; Tianxu, 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire'
+        }],
+        title: 'Fido'
+      }
+    }
+  }
+</script>
